68167039bcedf31144783b7ac7df770b71279ec5
Maximiliano Eekhout <maxeekhout93@gmail.com>
2023-08-16 18:25:08 +0100

    :white_small_square: Chapter 2: Templates (v0.2)

--- app/routes.py
+++ app/routes.py
@@ -1,6 +1,18 @@
 from app import app
+from flask import render_template
 
 @app.route('/')
 @app.route('/index')
 def index():
-    return "Hello, microBlog!"
+    user = {'username' : 'Max'}
+    posts = [
+        {
+            'author': {'username': 'Jhon'},
+            'body': 'Beautiful day in Tenerife'
+        },
+        {
+            'author': {'username': 'Susan'},
+            'body': 'The Avengers movie was so cool!'
+        }
+    ]
+    return render_template ('index.html', title='Home', user=user, posts=posts)
