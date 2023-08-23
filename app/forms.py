413304962a4f65fc0fdd2cffd0a35cbd16729b71
Maximiliano Eekhout <maxeekhout93@gmail.com>
2023-08-23 21:15:55 +0100

    :small_blue_diamond: Chapter 8: Followers (v0.8)

--- app/forms.py
+++ app/forms.py
@@ -3,7 +3,6 @@ from wtforms import StringField, PasswordField, BooleanField, SubmitField, TextA
 from wtforms.validators import ValidationError, DataRequired, Email, EqualTo, Length
 from app.models import User
 
-
 class LoginForm(FlaskForm):
     username = StringField('Username', validators=[DataRequired()])
     password = PasswordField('Password', validators=[DataRequired()])
@@ -42,3 +41,6 @@ class EditProfileForm(FlaskForm):
             user = User.query.filter_by(username=self.username.data).first()
             if user is not None:
                 raise ValidationError('Please use a different username.')
+            
+class EmptyForm(FlaskForm):
+    submit = SubmitField('Submit')
