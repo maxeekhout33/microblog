f72771bcb879715fb8b7070bf5ba11448cbdd78d
Maximiliano Eekhout <maxeekhout93@gmail.com>
2023-08-25 17:10:33 +0100

    :small_blue_diamond: Chapter 11: Facelift (v0.11)

--- app/__init__.py
+++ app/__init__.py
@@ -5,6 +5,7 @@ from flask_migrate import Migrate
 from flask_login import LoginManager
 from logging.handlers import SMTPHandler, RotatingFileHandler
 from flask_mail import Mail
+from flask_bootstrap import Bootstrap
 import logging
 import os
 
@@ -15,6 +16,7 @@ migrate = Migrate(app, db)
 login = LoginManager(app)
 login.login_view = 'login'
 mail = Mail(app)
+bootstrap = Bootstrap(app)
 
 if not app.debug:
     if app.config['MAIL_SERVER']:
